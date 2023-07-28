8b6b91417b839ac20888eabc3b0ffcae45b787bf
Doan Duc <dukinfotech@gmail.com>
2023-07-29 00:13:05 +0900

    add: middleware

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken")?.value;
+  if (request.nextUrl.pathname === "/login" && accessToken) {
+    return NextResponse.redirect(new URL("/admin", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/login",
+};
